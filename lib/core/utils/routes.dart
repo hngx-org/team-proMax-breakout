56c84662e620d25a1b8221fb0f15a71738a4532f
Benita <ezebenita6101@gmail.com>
2023-10-16 19:31:30 +0100

    Revert "User Authentication"
    This reverts commit 2d6230e3531e549df5aa11af5e37beeb9c8d8aaf.

--- lib/core/utils/routes.dart
+++
@@ -1,3 +0,0 @@
-const loginRoute = '/login/';
-const registerRoute = '/register/';
-const verifyEmailRoute = '/verifyEmail';
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,15 @@
-import 'package:bluck_buster/authentication/signup.dart';
 import 'package:bluck_buster/features/game/game_page.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'authentication/Signi.dart';
-import 'authentication/verify_email.dart';
-import 'core/utils/routes.dart';
 import 'features/menu/menu.view.dart';
 import 'features/splash/next.splash.view.dart';
 import 'features/splash/splash.view.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
+
   runApp(
     const MyApp(),
-  )
   );
 }
 
@@ -40,9 +32,6 @@ class MyApp extends StatelessWidget {
         '/nextSplash': (context) => const NextSplash(),
         '/menuScreen': (context) => const GameMenu(),
         '/gameScreen': (context) => const GamePage(),
-        loginRoute: (context) => LoginView(),
-        registerRoute: (context) => const RegisterView(),
-        verifyEmailRoute: (context) => const VerifyEmailView(),
       },
     );
   }
